9221427fa3d6331c80b2e0535fa1381de458d2ac
Rémi Dubois <packman@oxiame.net>
2015-12-25 18:41:31 +0100

    Cleaning app.js

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
