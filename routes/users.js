f53c2e185a2f28b233f8e22dd55d79fec9d291ea
Rémi Dubois <packman@oxiame.net>
2015-12-26 17:06:15 +0100

    Some tests inside routes

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,37 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     'use strict';
-    res.send('respond with a resource');
+    res.json({
+        body: req.body,
+        cookies: req.cookies,
+        ip: req.ip,
+        params: req.params,
+        query: req.query,
+        route: req.route,
+        client: {
+            user: req.get('X-Airlift-User'),
+            token: req.get('X-Airlift-Token')
+        },
+        xhr: req.xhr
+    });
+});
+
+router.get('/:name', function(req, res, next) {
+    'use strict';
+    res.json({
+        body: req.body,
+        cookies: req.cookies,
+        ip: req.ip,
+        params: req.params,
+        query: req.query,
+        route: req.route,
+        client: {
+            user: req.get('X-Airlift-User'),
+            token: req.get('X-Airlift-Token')
+        },
+        user: req.params.name,
+        xhr: req.xhr
+    });
 });
 
 module.exports = router;
